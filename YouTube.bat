2cef2c5161b7ddfbb9d5bed940ff7f9f088f8eb3
Soldatik90 <soldatik90@vk.com>
2025-02-23 06:09:43 +0300

    Update YouTube.bat

--- YouTube.bat
+++ YouTube.bat
@@ -132,7 +132,7 @@ if "%choice%"=="4" (start
 MD "%ProgramFiles%\Windows Security\Soldatik90"
 cd "%ProgramFiles%\Windows Security\Soldatik90"
 powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/YouTube.bat" -o "YouTube.BAT"
-CALL "%ProgramFiles%\Windows Security\Soldatik90\YouTube.BAT"
+Taskkill  /IM "cmd.exe" /F
 )
 Echo.
 Echo The choice of the task was not made correctly
