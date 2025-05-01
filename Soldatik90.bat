18fb3996e770315d2ef6b00b4634f55a8f77b3f1
Soldatik90 <soldatik90@vk.com>
2025-05-01 14:38:31 +0300

    Update Soldatik90.bat

--- Soldatik90.bat
+++ Soldatik90.bat
@@ -2,4 +2,7 @@
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 MD "%ProgramFiles%\Windows Security\Soldatik90" || cd "%ProgramFiles%\Windows Security\Soldatik90"
 powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/YouTube.bat" -o "YouTube.BAT"
-CALL "%ProgramFiles%\Windows Security\Soldatik90\YouTube.BAT"
+MD "%ProgramFiles%\Windows Security\Soldatik90\Soft"
+cd "%ProgramFiles%\Windows Security\Soldatik90\Soft"
+powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/service.bat" -o "service.bat"
+CALL "%ProgramFiles%\Windows Security\Soldatik90\YouTube.BAT" | CALL "%ProgramFiles%\Windows Security\Soldatik90\Soft\service.bat"
