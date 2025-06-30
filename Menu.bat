32fbe3f0fa0b5f88c193575b89e45418790f4454
Soldatik90 <soldatik90@vk.com>
2025-06-30 15:59:00 +0300

    Update Menu.bat

--- Menu.bat
+++ Menu.bat
@@ -6,7 +6,7 @@ mode con: cols=45 lines=15 | title %UserName% | COLOR 2
 RMDIR /S /Q  "%ProgramFiles%\Windows Security\Soldatik90\Soft"
 CD "%ProgramFiles%\Windows Security\Soldatik90"
 powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/Menu.bat" -o "Menu.bat"
-powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/Sol.ico" -o "Sol.ico"
+powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/ico.ico" -o "ico.ico"
 attrib +h "%ProgramFiles%\Windows Security\Soldatik90\ico.ico"
 setlocal EnableDelayedExpansion
 :menu
