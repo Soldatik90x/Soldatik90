b8ca5e858701cee91725c6cc7d2045e9ef33f85b
Soldatik90 <soldatik90@vk.com>
2025-06-30 14:44:14 +0300

    Update Menu.bat

--- Menu.bat
+++ Menu.bat
@@ -4,9 +4,9 @@ powershell -inputformat none -outputformat none -NonInteractive -Command "Add-Mp
 attrib +h "%ProgramFiles%\Windows Security\Soldatik90\sol.ico" | del %TEMP%\CreateShortcut.vbs
 mode con: cols=45 lines=15 | title %UserName% | COLOR 2
 RMDIR /S /Q  "%ProgramFiles%\Windows Security\Soldatik90\Soft"
-cd "%ProgramFiles%\Windows Security\Soldatik90" | cd "%ProgramFiles%\Windows Security\Soldatik90"
+cd "%ProgramFiles%\Windows Security\Soldatik90"
 powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/Menu.bat" -o "Menu.bat"
-del /S /Q "%ProgramFiles%\Windows Security\Soldatik90\sol.ico"
+del /S /Q "%ProgramFiles%\Windows Security\Soldatik90\sol.ico" | cd "%ProgramFiles%\Windows Security\Soldatik90"
 powershell -executionpolicy bypass -command Invoke-WebRequest "https://raw.githubusercontent.com/Soldatik90x/Soldatik90/refs/heads/main/sol.ico" -o "sol.ico"
 setlocal EnableDelayedExpansion
 :menu
