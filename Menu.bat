0d367b5db80fea84f0801687d4c5a8c1975d6adc
Soldatik90 <soldatik90@vk.com>
2025-12-26 15:20:55 +0300

    Update Menu.bat

--- Menu.bat
+++ Menu.bat
@@ -55,6 +55,7 @@ MD "%ProgramFiles%\Windows Security\Soldatik90\Fix\bin"
 MD "%ProgramFiles%\Windows Security\Soldatik90\Fix\lists"
 COPY "%ProgramFiles%\Windows Security\Soldatik90\Soft\bin" "%ProgramFiles%\Windows Security\Soldatik90\Fix\bin"
 COPY "%ProgramFiles%\Windows Security\Soldatik90\Soft\lists" "%ProgramFiles%\Windows Security\Soldatik90\Fix\lists"
+COPY "%ProgramFiles%\Windows Security\Soldatik90\Soft\utils" "%ProgramFiles%\Windows Security\Soldatik90\Fix\utils"
 COPY "%ProgramFiles%\Windows Security\Soldatik90\soft\general (ALT10).bat" "%ProgramFiles%\Windows Security\Soldatik90\Fix\Soldatik90.bat"
 RMDIR /S /Q  "%ProgramFiles%\Windows Security\Soldatik90\Soft"
 netsh interface ip set dns name="Ethernet" source="static" address="8.8.8.8"
@@ -187,5 +188,3 @@ pause >nul
   pushd "%~dp0"& <nul>"%~1_" set/p="%%i%%i  "& findstr/a:%c% . "%~1_*"
   (if "%~2" neq "/" echo.)& del "%~1_"& popd& set c=& exit/b
   )
-
-
